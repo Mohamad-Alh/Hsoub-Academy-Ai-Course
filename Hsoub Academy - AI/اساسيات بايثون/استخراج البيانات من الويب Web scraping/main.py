9b13f9225115d3fd8b854ae9eefbb743cd5ebf00
Mohamad-Alh <mohammedabdulkarimalh@gmail.com>
2025-08-14 18:43:47 +0300

    Add files via upload

--- Hsoub Academy - AI/اساسيات بايثون/استخراج البيانات من الويب Web scraping/main.py
+++ Hsoub Academy - AI/اساسيات بايثون/استخراج البيانات من الويب Web scraping/main.py
@@ -2,9 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import re
+from datetime import date
+from tabulate import tabulate 
+import json
+
+today = date.today().strftime('%d/%m/%Y')
 
 
-prefs = {
+def get_forecast_data():
+    
+    prefs = {
         "profile.default_content_setting_values": {
             "images": 2,
             "plugins": 2,
@@ -13,21 +20,18 @@ prefs = {
             "media_stream": 2,
         }
     }
-
-
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--log-level=3')
-options.add_experimental_option("prefs", prefs)
-
-driver = webdriver.Chrome(options=options)
-driver.get("https://world-weather.info/")
-
-def get_forecast_data():
     
-    driver.add_cookie({'name': 'celsius', 'value': '1','domain': 'world-weather.info'})
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--log-level=3')
+    options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://world-weather.info/")
+    
+    driver.add_cookie({'name': 'celsius', 'value': '1'})
     driver.refresh()
 
     html = driver.page_source
@@ -51,7 +55,29 @@ def get_forecast_data():
     return list(data)
 
 
-print(get_forecast_data())
+def get_forecast_txt():
+    data = get_forecast_data()
+    if data:
+        with open('output.txt', 'w') as f:
+            f.write("Popular Cities Forecast\n")
+            f.write(today + '\n')
+            f.write('='*58 + '\n' )
+            table  = tabulate(data,headers=["City" , "Temperature" , "Condition"] , tablefmt="pretty")
+            f.write(table)
+ 
+
+def get_forecast_json():
+    data = get_forecast_data()
+    if data:
+        cities = [{'city': city , 'temp': temps , 'condition': condition} for city , temps  , condition in data]
+        data_json = {'title': "Popular Cities Forecast", 'date': today , 'cities':cities}
+        with open('output.json', 'w') as f:
+            json.dump(data_json , f , ensure_ascii=False)
 
 
-    
+        
+
+if __name__ == "__main__":
+    get_forecast_txt()
+    get_forecast_json()
+
